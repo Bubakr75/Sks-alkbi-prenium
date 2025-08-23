87d674840d98735d78e8ac08d91ff01df712e968
Bubakr75 <boob_s4@hotmail.com>
2025-08-23 19:45:59 +0200

    Update MainActivity.kt

--- app/app/app/src/main/app/src/main/java/com/sks/alibi/MainActivity.kt
+++ app/app/app/src/main/app/src/main/java/com/sks/alibi/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sks.alibi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sks.alibi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "Hello SKS Alibi 👋"
     }
 }
